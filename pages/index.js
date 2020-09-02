6d39e3bc73958db93f15c2d24f627024ed59312f
Renato <renatoyufra@gmail.com>
2020-09-02 18:38:11 -0500

    Add Dynamics Routes

--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,33 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Alert from '../components/alert'
 import { getSortedPostsData } from '../lib/posts'
+import Date from '../components/date'
+import Link from 'next/link'
 
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{ siteTitle }</title>
+        <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Im a front-end web developer, social, kindness and a pet lover (Spanish/English). You can contact me on <a href="https://www.linkedin.com/in/renatoyufra/" target="_blank" >LinkedIn</a></p>
         <p>
           (This is a sample website - you'll be building a site like this on {' '} <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-        <Alert type="error">
-          miau
-        </Alert>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({id, date, title}) => (
+        <ul className={utilStyles.listItem}>
+          {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
@@ -39,7 +40,7 @@ export default function Home({ allPostsData }) {
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
-  
+
   return {
     props: {
       allPostsData
